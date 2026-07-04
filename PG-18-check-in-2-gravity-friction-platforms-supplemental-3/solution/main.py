b9e458ce8773666085fb1661671766789d641349
Jacob Anderson <jacobdanderson@gmail.com>
2026-07-04 15:16:10 -0400

    Apply coding standard to PyGames solutions

--- PG-18-check-in-2-gravity-friction-platforms-supplemental-3/solution/main.py
+++ PG-18-check-in-2-gravity-friction-platforms-supplemental-3/solution/main.py
@@ -1,12 +1,30 @@
+#################
+#   CONSTANTS   #
+#################
+
+VALUE_MULTIPLIER = 2
+VALUE_OFFSET = 1
+SAMPLE_VALUES = [3, 7, 11, 19]
+
+
+#################
+#   FUNCTIONS   #
+#################
+
 def transform(values: list[int]) -> list[int]:
-	result: list[int] = []
+	"""Transform each input value with the lesson formula"""
+	transformed_values: list[int] = []
+
+	# Transform each value independently so output order matches input order
 	for value in values:
-		result.append(value * 2 + 1)
-	return result
+		transformed_values.append(value * VALUE_MULTIPLIER + VALUE_OFFSET)
+
+	return transformed_values
 
 
 def main() -> None:
-	print(transform([3, 7, 11, 19]))
+	"""Print the transformed sample values"""
+	print(transform(SAMPLE_VALUES))
 
 
 if __name__ == "__main__":
